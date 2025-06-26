e0862c2f1eb070abfe01297ef96f7c5211ac3bcf
P105XY <p105xy85@gmail.com>
2025-06-26 21:26:17 +0900

    Initialize Commit

---
+++ Source/Mech/Public/MechGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MechGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECH_API AMechGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
